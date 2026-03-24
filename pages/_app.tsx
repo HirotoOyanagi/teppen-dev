8bde652c1330966d50b9c9b911ae7a36606fefc3
HirotoOyanagi <hiro.yanagi3204@gmail.com>
2026-03-25 00:37:42 +0900

    UIを参照できるように

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,10 @@ import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 import PortraitLock from '@/components/PortraitLock'
 
+if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
+  import('@locator/runtime')
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
